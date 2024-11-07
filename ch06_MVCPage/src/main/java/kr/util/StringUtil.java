b93499934a77e52453df45321ec9bb4a1fcfa7ff
704hj <704hj@naver.com>
2024-11-07 09:04:59 +0900

    241107

--- ch06_MVCPage/src/main/java/kr/util/StringUtil.java
+++ ch06_MVCPage/src/main/java/kr/util/StringUtil.java
@@ -23,4 +23,53 @@ public class StringUtil {
 		//JSP 경로 반환		
 		return "common/ajax_view.jsp";
 	}
+	
+	//HTML 태그를 허용하면서 줄바꿈
+	public static String useBrHtml(String str) {
+		if(str == null) return null;
+		
+		return str.replaceAll("\r\n", "<br>")
+				  .replaceAll("\r", "<br>")
+				  .replaceAll("\n", "<br>");
+	}
+	
+	//HTML 태그를 허용하지 않으면서 줄바꿈
+	public static String useBrNoHtml(String str) {
+		if(str == null)return null;
+		
+		return str.replaceAll("<", "&lt;")
+				  .replaceAll(">", "&gt;")
+				  .replaceAll("\r\n", "<br>")
+				  .replaceAll("\r", "<br>")
+				  .replaceAll("\n", "<br>");
+		
+	}
+	
+	//HTML 태그를 허용하지 않음 : 완전불허
+	public static String useNoHtml(String str) {
+		if(str == null) return null;
+		
+		return str.replaceAll("<", "&lt;")
+			  	  .replaceAll(">", "&gt;");
+	}
+	
+	//큰 따옴표 처리
+	public static String parseQuot(String str) {
+		if(str == null) return null;
+		
+		return str.replaceAll("\"", "&quot;");
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
